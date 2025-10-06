4470c7b4ce5ea3f070fd10aba9957aec44e04b7e
brunobernardesduarte <brunobernardesduarte@gmail.com>
2025-10-06 02:17:22 +0000

    fix(redirect): set overflow-y-hidden

--- nihon/src/components/ui/about/redirect.tsx
+++ nihon/src/components/ui/about/redirect.tsx
@@ -20,7 +20,7 @@ export default function Redirect( { title, url }: RedirectProps) {
     };
 
     return(
-        <div className="w-full">
+        <div className="w-full overflow-y-hidden">
             <div onClick={handleClick} className="flex items-center justify-between py-5 mx-15 transform transition duration-150 hover:scale-102">
                 <h1 className="text-secondary text-[20px] font-bold">{title}</h1>
                 <ArrowRightFromLine color="white" />

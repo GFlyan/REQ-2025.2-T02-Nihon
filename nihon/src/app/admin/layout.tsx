977331fa34aab103538f356f5a441b993e86613c
Flyan <guilhermeflyan.22@gmail.com>
2025-11-04 16:24:32 -0300

    Login Admin Page
    Changes on Login Admin Page.

--- nihon/src/app/admin/layout.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const metadata = {
-  title: "Admin - Login",
-};
-
-export default function AdminLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
-}
